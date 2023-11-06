916ac5107fff4b5700466ec00d11986c185198b2
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-11-06 10:45:49 -0600

    add react router package, add homebar component to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import Router from "./Router.jsx"
+import { HomeBar } from './HomeBar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HomeBar></HomeBar>
     <Router />
   </React.StrictMode>,
 )
