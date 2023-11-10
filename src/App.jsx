b2ab20498a100a36fc2f1509d7684e05d6c54831
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-11-09 23:35:40 -0600

    add content to the front page

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,20 @@ function App(props) {
 
   return (
     <>
-      
-      <div className='imgInfo'>This is a place holder for home page image and info</div>
+      <div className='homePageTitle'>Fashion and Taste That Never Get Old</div>
+      <div className='menTitle'>MEN</div>
+      <div className='homeImages men'>
+        <img src="/src/assets/front1.jpg" alt="home image man1" />
+        <img src="src/assets/front2.jpg" alt="home image man2" />
+        <img src="src/assets/front3.jpg" alt="home image man3" />
+      </div>
+      <div className='womenTitle'>WOMEN</div>
+      <div className='homeImages women'>
+        <img src="src/assets/front4.jpg" alt="home image woman1" />
+        <img src="src/assets/front5.jpg" alt="home image woman2" />
+        <img src="src/assets/front6.jpg" alt="home image woman3" />
+      </div>
+      <footer className='footer'>&copy; 2023 All Rights Reserved</footer>
     </>
   )
 }
