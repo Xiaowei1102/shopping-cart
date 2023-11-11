710b3b1403f02ca212e09477fa5876243bf295b1
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-11-10 22:38:26 -0600

    img originally in assets folder not shown on deployment; move them to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ function App(props) {
       <div className='homePageTitle'>Fashion and Taste That Never Get Old</div>
       <div className='menTitle'>MEN</div>
       <div className='homeImages men'>
-        <img src="/src/assets/front1.jpg" alt="home image man1" />
-        <img src="src/assets/front2.jpg" alt="home image man2" />
-        <img src="src/assets/front3.jpg" alt="home image man3" />
+        <img src="/front1.jpg" alt="home image man1" />
+        <img src="/front2.jpg" alt="home image man2" />
+        <img src="/front3.jpg" alt="home image man3" />
       </div>
       <div className='womenTitle'>WOMEN</div>
       <div className='homeImages women'>
-        <img src="src/assets/front4.jpg" alt="home image woman1" />
-        <img src="src/assets/front5.jpg" alt="home image woman2" />
-        <img src="src/assets/front6.jpg" alt="home image woman3" />
+        <img src="/front4.jpg" alt="home image woman1" />
+        <img src="/front5.jpg" alt="home image woman2" />
+        <img src="/front6.jpg" alt="home image woman3" />
       </div>
       <footer className='footer'>&copy; 2023 All Rights Reserved</footer>
     </>
